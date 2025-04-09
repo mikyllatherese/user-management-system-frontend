73c3ca7a3a788b4d9d7081bcd388864029849346
Mikylla Therese Dinauanao <mikyllatheresed@gmail.com>
2025-04-09 23:25:58 +0800

    resolved conflicts

--- src/app/admin/accounts/accounts-routing.module.ts
+++ src/app/admin/accounts/accounts-routing.module.ts
@@ -1,16 +1,10 @@
-<<<<<<< HEAD
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
-=======
-import { NgModule } from "@angular/core";
-import { Routes, RouterModule } from "@angular/router";
->>>>>>> Dinauanao-tester-functional-testing
 
 import { ListComponent } from './list.component';
 import { AddEditComponent } from './add-edit.component';
 
 const routes: Routes = [
-<<<<<<< HEAD
   { path: '', component: ListComponent },
   { path: 'add', component: AddEditComponent },
   { path: 'edit/:id', component: AddEditComponent }
@@ -21,15 +15,3 @@ const routes: Routes = [
   exports: [RouterModule]
 })
 export class AccountsRoutingModule { }
-=======
-    {path: '', component: ListComponent},
-    {path: 'add', component: AddEditComponent},
-    {path: 'edit/:id', component: AddEditComponent},
-];
-
-@NgModule({
-    imports: [RouterModule.forChild(routes)],
-    exports: [RouterModule]
-})
-export class AccountsRoutingModule { }
->>>>>>> Dinauanao-tester-functional-testing
